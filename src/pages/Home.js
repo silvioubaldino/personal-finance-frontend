ed61a20427d6976cf2af74647d7db5823abfb4ae
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-02-02 15:04:57 -0400

    Adiciona proteção nas páginas se estiver sem token

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,21 @@ import ResumedReport from "../components/ResumedReport";
 import AccordionBuild from "../components/AccordionBuild";
 
 import "../style/pages/home.css";
+import { useEffect } from "react";
+import { readStorage } from "../utils/handleLocalStorage";
+import { useHistory } from "react-router";
 
 const Home = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = readStorage();
+    console.log(token);
+    if (!token || token.length < 1) {
+      history.push('/login');
+    }
+  }, []);
+
   return (
     <div className="container-home">
       <header className="container-header">
