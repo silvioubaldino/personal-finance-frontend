9d882004126c9ea236b32d72fa290f91790299f9
Vitor Lichoti <vitorlichoti@gmail.com>
2023-04-03 20:21:07 -0400

    ---

--- src/components/Movements.jsx
+++ src/components/Movements.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 import { Container } from 'react-bootstrap';
